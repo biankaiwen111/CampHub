fae28e6c7a61f93319ff93cc0d8fe0c9a0e45d29
biankaiwen111 <biankaiwen111@outlook.com>
2021-07-04 18:35:21 -0700

    fix app

--- app.js
+++ app.js
@@ -87,6 +87,7 @@ const scriptSrcUrls = [
   "https://kit.fontawesome.com/",
   "https://cdnjs.cloudflare.com/",
   "https://cdn.jsdelivr.net",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css",
 ];
 const styleSrcUrls = [
   "https://kit-free.fontawesome.com/",
@@ -95,6 +96,7 @@ const styleSrcUrls = [
   "https://api.tiles.mapbox.com/",
   "https://fonts.googleapis.com/",
   "https://use.fontawesome.com/",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css",
 ];
 const connectSrcUrls = [
   "https://api.mapbox.com/",
