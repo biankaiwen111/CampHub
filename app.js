1046b75a3558baf2435aa39a6b2e59c650c766df
biankaiwen111 <biankaiwen111@outlook.com>
2021-07-04 22:52:02 -0700

    fix app

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ const campgroundRoutes = require("./routes/campgrounds");
 const reviewRoutes = require("./routes/reviews");
 
 //const MongoDBStore = require("connect-mongo")(session);
+const MongoStore = require("connect-mongo");
 
 const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/yelp-camp";
 
